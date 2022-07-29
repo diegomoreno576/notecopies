8152e2686d79bb448ae55b56182fc40fc7f5ea77
diego576 <diegomoreno576@gmail.com>
2022-07-29 12:39:49 +0200

    primer commit

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,20 @@
+class MembersController < ApplicationController 
+    before_action :authenticate_user!
+
+    def show
+
+        user = get_user_from_token
+        render json: {
+            message: "si ves esto estas logueado",
+            user: user
+        }
+    end
+
+    private 
+    def get_users_from_token
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+    Rails.application.credencials.devise[:jwt_secret_key]).first
+    user_id = jwt_payload['sub']
+    user = User.find(user_id.to_s)
+    end
+end
