d16d4a757dc89dacbb81eef102c3f4d8630aafba
diego576 <diegomoreno576@gmail.com>
2022-07-31 10:20:59 +0200

    error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,26 +1,22 @@
-class Users::SessionsController < Devise::SessionsController
+class Users::RegistrationsController < Devise::RegistrationsController
     respond_to :json
   
     private
   
-    def respond_with(_resource, _opts = {})
+    def respond_with(resource, _opts = {})
+      register_success && return if resource.persisted?
+  
+      register_failed
+    end
+  
+    def register_success
       render json: {
-        message: 'You are logged in.',
+        message: 'Signed up sucessfully.',
         user: current_user
       }, status: :ok
     end
   
-    def respond_to_on_destroy
-      log_out_success && return if current_user
-  
-      log_out_failure
-    end
-  
-    def log_out_success
-      render json: { message: 'You are logged out.' }, status: :ok
-    end
-  
-    def log_out_failure
-      render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
+    def register_failed
+      render json: { message: 'Something went wrong.' }, status: :unprocessable_entity
     end
   end
